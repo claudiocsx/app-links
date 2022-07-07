6e691bf5555582ebd6345591a4a40c39f8ac0d00
unknown <claudiosasuke@gmail.com>
2022-07-06 22:15:48 -0300

    app

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('claudio')
+
+addEventListener("DOMContentLoaded", () => {
+
+  VANTA.GLOBE({
+  el: "#vanta",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 800.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundAlpha: 0.0
+})
+
+})
